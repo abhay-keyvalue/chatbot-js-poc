59d1fef59b801260ecc7bd7a1459531634b5e1f5
abhay-keyvalue <abhay-keyvalue@users.noreply.github.com>
2024-11-20 12:52:40 +0000

    deploy: 374f4f266c02451f63795cbe4ae3cdedb944bd80

--- dist/umd/dist/components/Table.d.ts
+++
@@ -1,2 +0,0 @@
-import type { TableComponentProps } from '@types';
-export declare const Table: ({ columns, rows }: TableComponentProps) => import("preact").JSX.Element;
--- dist/umd/dist/components/chatBubble/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ChatBubbleProps } from '@types';
-import './styles.css';
-/**
- * Represents a chat bubble component.
- * @param {ChatBubbleProps} props - The props for the chat bubble component.
- * @returns {JSX.Element} The rendered chat bubble component.
- */
-declare const ChatBubble: (props: ChatBubbleProps) => import("preact").JSX.Element;
-export default ChatBubble;
