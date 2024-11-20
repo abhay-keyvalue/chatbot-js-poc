aafc97132a49107c6689603a53e4084a6df505ea
abhay-keyvalue <abhay-keyvalue@users.noreply.github.com>
2024-11-20 12:59:41 +0000

    deploy: d4ce893316d8eabb4a6cda449839c9656537ea30

--- dist/umd/dist/components/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ChatBubble from './chatBubble';
-import ChatHeader from './chatHeader';
-import ChatInput from './chatInput';
-export { ChatBubble, ChatHeader, ChatInput };
--- dist/umd/dist/constants/translation.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const en: {
-    chatbot_title: string;
-    chatbot_placeholder: string;
-    error_message_no_response: string;
-    error_message_generic: string;
-    empty_screen_message: string;
-};
