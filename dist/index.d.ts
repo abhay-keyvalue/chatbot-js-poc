fbf05d98bb65a9de150c7520eb1aab0539bdf2e4
abhay-keyvalue <abhay-keyvalue@users.noreply.github.com>
2024-11-20 12:41:06 +0000

    deploy: bd5cc6b120b28f18e11d83814d88c18d131d1679

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @file This file entry p[oint of sdf contains the implementation of the ChatBot class and its dependencies.
+ */
+import type { ChatBotOptions } from '@types';
+/**
+ * Represents a ChatBot instance.
+ */
+export declare class ChatBot {
+    private theme;
+    private apiKey;
+    private agentType;
+    /**
+     * Creates a new instance of ChatBot.
+     * @param options - The options for configuring the ChatBot instance.
+     */
+    constructor(props: ChatBotOptions);
+    /**
+     * Initializes the user interface for the ChatBot.
+     * Creates a container element and renders the ChatBotUI component inside it.
+     */
+    private initUI;
+}
