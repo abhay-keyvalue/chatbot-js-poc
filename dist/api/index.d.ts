9519aa07e5368ad97ea8e898b1a421b77ce986e3
austimw <austimw@users.noreply.github.com>
2024-11-22 06:08:11 +0000

    deploy: d48f5eb26a49634af4336f6bb6cdcdbec3dad043

--- dist/api/index.d.ts
+++ dist/api/index.d.ts
@@ -9,11 +9,9 @@ import type { MessageData } from '@types';
  * @param url - The URL to make the API call to.
  * @param method - The HTTP method to use for the API call. Default is 'POST'.
  * @param body - The request body for the API call. Default is an empty object.
- * @param retries - The number of retries.
- * @param delay - Delay between retries in milliseconds.
  * @returns A Promise that resolves to the response data or an error message.
  */
-export declare function callApi(url: string, method?: HttpMethodOptions, body?: {}, retries?: number, delay?: number): Promise<any>;
+export declare function callApi(url: string, method?: HttpMethodOptions, body?: {}): Promise<any>;
 /**
  * Gets the bot response for the given user message by establishing a server-sent event (SSE) connection.
  * @param userMessage - The user message to send to the bot.
@@ -21,4 +19,4 @@ export declare function callApi(url: string, method?: HttpMethodOptions, body?: 
  * @param onStreamMessageError - A callback function to handle SSE connection errors.
  * @returns A Promise that resolves when the SSE connection is established.
  */
-export declare const getBotResponse: (userMessage: string, onStreamMessage: (event: MessageData) => void, onStreamMessageError: (error: string) => void) => Promise<void>;
+export declare const getBotResponse: (userMessage: string, apiUrl: string, onStreamMessage: (event: MessageData) => void, onStreamMessageError: (error: string) => void) => Promise<void>;
